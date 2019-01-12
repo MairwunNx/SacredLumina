75d79dbf060c04c018a4bc13702d8aa95cb0c8c8
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2019-01-12 23:08:56 +0300

    Optimized Getting Bytes of Hero positions.

--- src/utils/ByteUtils.js
+++ src/utils/ByteUtils.js
@@ -1,23 +1,31 @@
 import { getHexadecimalArray } from './HexUtils'
 
 /**
- * Return converted decimal number to Int32 type with ByteOrder LittleEndian.
+ * Return converted array of decimal number to array Int32 type with ByteOrder LittleEndian.
  *
  * @param  {string} path - Path to need file.
- * @param  {number} offset - decimal byte offset in file.
+ * @param  {number} offsetX - decimal byte offsetXPos in file.
+ * @param  {number} offsetY - decimal byte offsetYPos in file.
  * @param  {number} length - need data length, default 8 = Int32 type.
- * @return {number} Converted decimal number to Int32 type.
+ * @return {Array} Converted array of decimal number to Int32 type.
  *
  * @static
  */
-export function getNumberFromBytes (path, offset, length = 8) {
+export function getNumberFromBytes (path, offsetX, offsetY, length = 8) {
   let byteArray = getHexadecimalArray(path)
-  let arrayBuffer = new ArrayBuffer(length)
-  let dataView = new DataView(arrayBuffer)
+  let arrayBufferXPos = new ArrayBuffer(length)
+  let dataViewX = new DataView(arrayBufferXPos)
+
+  let arrayBufferYPos = new ArrayBuffer(length)
+  let dataViewY = new DataView(arrayBufferYPos)
 
   for (let i = 0; i < 5; i++) {
-    dataView.setUint8(i, byteArray[offset + i])
+    dataViewX.setUint8(i, byteArray[offsetX + i])
   }
 
-  return dataView.getInt32(0, true)
+  for (let i = 0; i < 5; i++) {
+    dataViewY.setUint8(i, byteArray[offsetY + i])
+  }
+
+  return [dataViewX.getInt32(0, true), dataViewY.getInt32(0, true)]
 }
