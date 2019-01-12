512c8941e287cf31f43fe1517f801b5ad499ac1d
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2019-01-12 21:37:49 +0300

    Added Store Data for gladX and gladY Positions.

--- src/store/base.js
+++ src/store/base.js
@@ -1,16 +1,28 @@
 export default {
   state: {
-    directoryPath: 'undefined'
+    directoryPath: 'undefined',
+    gladPageXPos: 'reading value ...',
+    gladPageYPos: 'reading value ...'
   },
   mutations: {
     directoryPathMutation (state, payload) {
       state.directoryPath = payload
+    },
+    gladPageXPosMutation (state, payload) {
+      state.gladPageXPos = payload
+    },
+    gladPageYPosMutation (state, payload) {
+      state.gladPageYPos = payload
     }
   },
   actions: {
-    setDirectoryPath: ({ commit }, payload) => commit('directoryPathMutation', payload)
+    setDirectoryPath: ({ commit }, payload) => commit('directoryPathMutation', payload),
+    setGladPageXPos: ({ commit }, payload) => commit('gladPageXPosMutation', payload),
+    setGladPageYPos: ({ commit }, payload) => commit('gladPageYPosMutation', payload)
   },
   getters: {
-    getDirectoryPath: (state) => state.directoryPath
+    getDirectoryPath: (state) => state.directoryPath,
+    getGladPageXPos: (state) => state.gladPageXPos,
+    getGladPageYPos: (state) => state.gladPageYPos
   }
 }
