c7889a2f40924cd334569e858bd0173dc0cbad4c
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2019-01-13 18:57:40 +0300

    Added ability to Save Sera data.

--- src/core/SaveData.js
+++ src/core/SaveData.js
@@ -8,4 +8,5 @@ import { putInt32DataInFile } from '../utils/ByteUtils'
  */
 export function saveData () {
   putInt32DataInFile(store.getters.getDirectoryPath + '\\TYPE_NPC_GLADIATOR\\startcode.bin', 551846, 551851, store.getters.getGladPageXPos, store.getters.getGladPageYPos)
+  putInt32DataInFile(store.getters.getDirectoryPath + '\\TYPE_NPC_SERAPHIM\\startcode.bin', 550551, 550556, store.getters.getSeraPageXPos, store.getters.getSeraPageYPos)
 }
